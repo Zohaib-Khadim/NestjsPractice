3c7b7941b24bb32b9d7e3c4dc1633c807e1275db
Muhammad-Zohaib <zaibimalik7994@gmail.com>
2025-10-08 14:41:25 +0500

    resolved the issue

--- src/auth/mongo-auth/mongo-auth.module.ts
+++ src/auth/mongo-auth/mongo-auth.module.ts
@@ -8,18 +8,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JWTStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [ConfigModule,MongooseModule.forFeature([{name:Worker.name, schema:WorkerSchema}]),JwtModule.registerAsync({
+  imports: [MongooseModule.forFeature([{name:Worker.name, schema:WorkerSchema}]),JwtModule.registerAsync({
     imports: [ConfigModule],
     inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        const secret = configService.get<string>('JWT_SECRET');
-        console.log('JWT Secret inside factory:', secret); // 👈 Add this line here
-
-        return {
-          secret,
-          signOptions: { expiresIn: '1h' },
-        };
-      },
+    useFactory: async (config:ConfigService ) => ({
+      secret: config.get<string>('JWT_SECRET'),
+      signOptions: { expiresIn: '1h' },   
+    }),
   })],
   controllers: [MongoAuthController],
   providers: [MongoAuthService, JWTStrategy]
